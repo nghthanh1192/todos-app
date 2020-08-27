fcec73de9828f1f151d2e204a7b0673bb4978c7a
Thanh Nguyen <hthanh1192@gmail.com>
2020-08-27 13:50:24 +0700

    add todos

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,15 +9,21 @@
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Todos App</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-          <div class="navbar-nav">
-            <a class="nav-item nav-link active" href="/todos">Todos <span class="sr-only">(current)</span></a>
-          </div>
-        </div>
+      <a class="navbar-brand" href="/">Todos App</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+    
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/todos">Todos <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item active">
+            <a class="nav-link" href="/new-todos">Create todos <span class="sr-only">(current)</span></a>
+          </li>
+        </ul>
+      </div>
     </nav>
     <div class="container">
         @yield('content')
--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/todos', 'TodosController@index');
-Route::get('/todos/{todo}', 'TodosController@show');
+Route::get('todos', 'TodosController@index');
+Route::get('todos/{todo}', 'TodosController@show');
+Route::get('new-todos', 'TodosController@create');
+Route::post('store-todos', 'TodosController@store');
